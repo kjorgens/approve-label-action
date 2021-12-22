ae647492b5d21ad323c371efdb89235f84b83fe7
kjorgens <kjorgens@yahoo.com>
2021-12-22 12:22:12 -0700

    fix: testing labels

--- approve-label.js
+++ approve-label.js
@@ -19,286 +19,48 @@ const getTeamList = `query($teamSlug: String!, $owner: String!) {
   }
 }`;
 
-const getRepoIdQuery = `query($repo: String!, $owner: String!) {
-  repository(owner: $owner, name: $repo) {
-    name
-    id
-  }
-}`;
-
-const createCommentMutation = `mutation($prId: ID!, $commentBody: String!) {
-  addComment(input:{subjectId: $prId, body: $commentBody}) {
-    commentEdge {
-      node {
-        createdAt
-        body
-      } 
-    }
-    subject {
-      id
-    }
-  }
-}`;
-
-const createLabelMutation = `mutation($color: String!, $name: String!, $description: String!, $repositoryId: ID!) {
-  createLabel(input:{color: $color, name: $name, description: $description, repositoryId: $repositoryId}) {
-    label {
-       name
-       id
-    }   
-  }
-}`;
-
-const addLabelsMutation = `mutation($labelIds: [ID!]!, $labelableId: ID!) {
-  addLabelsToLabelable(input:{labelIds: $labelIds, labelableId: $labelableId}) {
-     labelable {
-      labels(first: 50) {
-        nodes {
-          name
-        }
-      }
-    }
-  }
-}`;
-
-const removeLabelsMutation = `mutation($labelIds: [ID!]!, $labelableId: ID!) {
-  removeLabelsFromLabelable(input:{labelIds: $labelIds, labelableId: $labelableId}) {
-     labelable {
-      labels(first: 50) {
-        nodes {
-          name
-        }
-      }
-    }
-  }
-}`;
-
-const removeAllLabelsMutation = `mutation($labelableId: ID!) {
-  clearLabelsFromLabelable(input:{labelableId: $labelableId}) {
-     labelable {
-      labels(first: 50) {
-        nodes {
-          name
-        }
-      }
-    }
-  }
-}`;
-
-const getRepoLabels = `query($repoId: ID!) { 
-  node(id: $repoId) {
-    ... on Repository {
-      labels(first: 50) {
-        totalCount
-        nodes {
-          name
-          id
-        }
-      }
-    }
-  }
-}`;
-
-async function getTeamMembers(login, team) {
-  return await graphql(addLabelsMutation, {
-    teamSlug: team,
-    owner: login,
-    headers: {
-      authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`
-    }
-  });
-}
-
-async function addLabelToTable(owner, repoId, labelName, color, labelDescription) {
-  try {
-    return await graphql(createLabelMutation, {
-      color: color || 'FBCA04',
-      description: labelDescription || '',
-      name: labelName,
-      repositoryId: repoId,
-      headers: {
-        authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`,
-        accept: 'application/vnd.github.bane-preview+json'
-      }
-    });
-  } catch(err) {
-    console.log(err);
-  }
-}
-
-async function findPrId(owner, repo, prNumber) {
-  const pullRequest = await graphql(getPRIdQuery, {
-    repo: repo,
+async function getTeamArray(teamName, owner, sender) {
+  const members = await graphql(getTeamList, {
+    teamSlug: teamName,
     owner: owner,
-    prNumber: prNumber,
-    headers: {
-      authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`,
-    }
-  });
-
-  return pullRequest.repository.pullRequest.id;
-}
-
-async function findRepoId(owner, repo) {
-  const repoIdInfo = await graphql(getRepoIdQuery, {
-    owner: owner,
-    repo: repo,
     headers: {
       authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`
     }
   });
 
-  return (repoIdInfo.repository.id);
-}
-
-async function findLabelId(owner, repoId, labelName) {
-  const labels = await graphql(getRepoLabels, {
-    repoId: repoId,
-    headers: {
-      authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`,
-      accept: 'application/vnd.github.bane-preview+json'
-    }
-  });
-
-  const labelFound = labels.node.labels.nodes.find((label) => {
-    return label.name === labelName;
-  });
-
-  console.log(labelFound);
-  if (labelFound) {
-    return labelFound.id;
-  }
-  throw new Error(`label ${labelName} not found`);
-}
-
-async function createPrLabel(owner, repo, prNumber, labelName, labelColor, labelDescription) {
-  let labelId;
-  let repoId;
-  let prId;
-  try {
-    repoId = await findRepoId(owner, repo);
-    prId = await findPrId(owner, repo, prNumber);
-  } catch(err) {
-    core.setFailed(`failure creating label = ${err.message}`);
+  if (members.organization.team === null) {
+    core.setFailed(`can't find ${teamName} in ${owner} organization`);
     process.exit(1);
   }
-  try {
-    labelId = await findLabelId(owner, repoId, labelName);
-  } catch (err) {
-    if (err.message === 'label not found') {
-      console.log(`adding ${labelName} to ${repo}`);
-      const results =  await addLabelToTable(owner, repoId, labelName, labelColor, labelDescription);
-      labelId = results.createLabel.label.id;
-    }
-  }
-
-  try {
-    const labels = await addLabels(prId, labelId);
-  } catch (err) {
-    core.setFailed(`failure creating label = ${err.message}`);
-    process.exit(1);
-  }
-  console.log(`label ${labelName} added to ${prNumber}`);
-  process.exit(0);
-}
-
-async function removePrLabels(owner, repo, prNumber, labelNames) {
-  let repoId;
-  let prId;
-  try {
-    repoId = await findRepoId(owner, repo);
-    prId = await findPrId(owner, repo, prNumber);
-  } catch(err) {
-    core.setFailed(`failure finding pull request ${prNumber}: ${err.message}`);
-    process.exit(1);
-  }
-
-  const labelIds = await Promise.all(labelNames.map(async (label) => {
-    try {
-      return await findLabelId(owner, repoId, label.trim());
-    } catch (err) {
-      if (err.message === 'label not found') {
-        console.log(`label ${label} does not exist on PR number ${prNumber}`);
-      }
-      return null;
-    }
-  }));
-
-  const validIds = labelIds.filter((id) => {
-    return id;
-  });
-
-  try {
-    const labels = await removeLabelsFromPr(prId, validIds);
-  } catch (err) {
-    core.setFailed(`failure removing label = ${err.message}`);
-    process.exit(1);
-  }
-  process.exit(0);
-}
-
-async function createPrComment(owner, repo, prNum, commentBodyText) {
-  const prId = await findPrId(owner, repo, prNum);
-
-  return await graphql(createCommentMutation, {
-    prId: prId,
-    commentBody: commentBodyText,
-    owner: owner,
-    repo: repo,
-    headers: {
-      authorization: `token ${process.env.GH_TOKEN || core.getInput('github-token')}`
-    }
-  });
+  return members.organization.team.members.edges.find((member) => {
+    return member.node.login === sender;
+  })
 }
 
 (async () => {
   try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(payload);
-    console.log(github.context.payload.action);
-    console.log(github.context.payload.user.name);
-    console.log(github.context.payload.label.name);
+    const sender = github.context.payload.sender.login;
+    const expectedLabel = 'Label Action';
+    const org = github.context.payload.organization.login;
+    const teams = core.getInput('valid-approval-teams');
+    const teamSlugs = teams.split(',');
+    let validApprover = false;
+    await Promise.all(teamSlugs.map(async (team) => {
+      try {
+        const approvers = await getTeamArray(team.trim(), org, sender);
+        if (approvers) {
+          validApprover = true;
+        }
+      } catch(err) {
+        console.log(err.message);
+      }
+    }));
 
-    let repoName;
-    let prNumber;
-    let repoOwner;
-    let labelName;
-    let labelColor;
-    let labelDescription;
-    let commentAddString;
-    let commentRemoveString;
-
-    // if (github.context.payload.action === 'created' && github.context.payload.comment !== undefined) {
-    //   commentString = github.context.payload.comment.body;
-    //   repoName = github.context.payload.repository.name;
-    //   prNumber = github.context.payload.issue.number;
-    //   repoOwner = github.context.payload.organization.login;
-    //   labelName = core.getInput('label-name');
-    //   labelColor = core.getInput('label-color') || 'FBCA04';
-    //   labelDescription = core.getInput('label-description') || '';
-    //   commentAddString = core.getInput('comment-trigger-add') || 'add labels';
-    //   commentRemoveString = core.getInput('comment-trigger-remove') || 'remove labels';
-    //   commentRemoveAllString = core.getInput('comment-trigger-remove-all') || 'remove all labels';
-    //
-    //   if (commentString.match(new RegExp(commentAddString, 'gi'))) {
-    //     if (commentString.length > commentAddString.length + 2) {
-    //       labelName = commentString.slice(commentAddString.length + 1);
-    //     }
-    //     await createPrLabel(repoOwner, repoName, prNumber, labelName.trim(), labelColor, labelDescription);
-    //   }
-    //   if (commentString.match(new RegExp(commentRemoveString, 'gi'))) {
-    //     if (commentString.length > commentRemoveString.length + 2) {
-    //       labelName = commentString.slice(commentRemoveString.length + 1);
-    //       labelNames = labelName.split(',');
-    //     } else {
-    //       labelNames =  labelName;
-    //     }
-    //     await removePrLabels(repoOwner, repoName, prNumber, labelNames);
-    //   }
-    //   if (commentString.match(new RegExp(commentRemoveAllString, 'gi'))) {
-    //     await removeAllLabelsFromPr(repoOwner, repoName, prNumber);
-    //   }
-    // }
+    if (!validApprover || expectedLabel !== 'Label Action') {
+      core.setFailed(`${sender} is not a valid approver for label ${expectedLabel}`);
+      process.exit(1);
+    }
+    process.exit(0);
   } catch (error) {
     core.setFailed(error.message);
     process.exit(1);
